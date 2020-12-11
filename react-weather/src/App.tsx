d7ace4b1050d02b07a43c4186e16a119bd1e7626
antoniopagotto121@gmail.com <antoniopagotto121@gmail.com>
2020-12-10 23:01:29 -0300

    Layout

--- react-weather/src/App.tsx
+++ react-weather/src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+
+const api = {
+  key: "2dd1b7f777237a53fa47dd63b7cc873c",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 
 function App() {
+
+    const[query, setQuery] = useState('');
+    const[weather, setWeather] = useState('');
+
+  const dateBuilder = (d: any) => {
+    let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    let days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Pesquisar..." />
+        </div>
+        <div>
+          <div className="location-box">
+            <div className="location">Itajubá, MG</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              25ºC
+            </div>
+            <div className="weather">
+              Limpo
+          </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
